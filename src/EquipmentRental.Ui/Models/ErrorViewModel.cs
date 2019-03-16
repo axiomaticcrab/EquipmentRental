faea874a33e071cd368ce2b152214d890109512b
ali.hekimoglu <ali.hekimoglu@morhipo.com>
2019-03-16 20:13:00 +0300

    Create solution and add web ui

---
+++ src/EquipmentRental.Ui/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EquipmentRental.Ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
