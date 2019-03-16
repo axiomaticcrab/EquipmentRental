091838ac64c336a6c3e2ee58c52f2eed4b31c748
ali.hekimoglu <ali.hekimoglu@morhipo.com>
2019-03-16 21:29:30 +0300

    Add Equipment service

---
+++ src/EquipmentRental.Services.EquipmentService/Controllers/EquipmentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EquipmentRental.Services.EquipmentService.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EquipmentRental.Services.EquipmentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipmentController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Equipment>> Get()
+        {
+            return new List<Equipment>
+            {
+                new Equipment {Id = 1, Name = "Caterpillar bulldozer", EquipmentType = EquipmentType.Heavy},
+                new Equipment {Id = 2, Name = "KamAZ truck", EquipmentType = EquipmentType.Regular},
+                new Equipment {Id = 3, Name = "Komatsu crane", EquipmentType = EquipmentType.Heavy},
+                new Equipment {Id = 4, Name = "Volvo steamroller", EquipmentType = EquipmentType.Regular},
+                new Equipment {Id = 5, Name = "Bosh jackhammer", EquipmentType = EquipmentType.Specialized}
+            };
+        }
+    }
+}
---
+++ src/EquipmentRental.Services.EquipmentService/Model/Equipment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EquipmentRental.Services.EquipmentService.Model
+{
+    public class Equipment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EquipmentType EquipmentType { get; set; }
+    }
+
+    public enum EquipmentType
+    {
+        Heavy = 10,
+        Regular = 20,
+        Specialized = 30
+    }
+}
